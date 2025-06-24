2212be664211d3738ba136db4d42fe910644d0a8
Arefin  Mojumder <137310857+Arefin201@users.noreply.github.com>
2025-06-24 12:07:54 +0600

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,39 +10,23 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-<<<<<<< HEAD
-
     use HasApiTokens, Notifiable, HasFactory;
 
     protected $fillable = ['name', 'email', 'password', 'profile_image'];
 
-
-=======
-    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
->>>>>>> 5f4ca15e0088d935f8e297467f94f29c41419ad0
+
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-<<<<<<< HEAD
 
     protected $casts = [
         'email_verified_at' => 'datetime',
@@ -54,14 +38,4 @@ class User extends Authenticatable
     }
 }
 
-=======
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-}
->>>>>>> 5f4ca15e0088d935f8e297467f94f29c41419ad0
+
